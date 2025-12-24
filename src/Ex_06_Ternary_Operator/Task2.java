fe265af77915b573e076cb76e947cab02b06e00f
Chaitany_Chavan <chaitanychavan993@gmail.com>
2025-12-24 08:48:58 +0530

    all new programs

--- src/Ex_06_Ternary_Operator/Task2.java
+++ src/Ex_06_Ternary_Operator/Task2.java
@@ -5,6 +5,8 @@ public class Task2 {
         int n1=2;
         int n2=9;
         int n3=-11;
-//        int Result = (Max Of Three n1 > n2 > n3)? ():();
+
+        int max=(n1> n3)?(n1>n2?n1:n2):(n2>n3?n2:n3);
+        System.out.println("MAX OF THREE:" + max);
     }
 }
