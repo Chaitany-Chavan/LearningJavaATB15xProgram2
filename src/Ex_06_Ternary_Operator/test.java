5f2aa93c84778e5cea2a7ab763b3135e5fd827ea
Chaitany_Chavan <chaitanychavan993@gmail.com>
2026-01-03 23:14:18 +0530

    03 jan 26 update

---
+++ src/Ex_06_Ternary_Operator/test.java
@@ -0,0 +1,9 @@
+package Ex_06_Ternary_Operator;
+
+public class test {
+    static void main() {
+        int a=10;
+        int b=6;
+        int c = a+b*5; System.out.println(c);
+    }
+}
---
+++ src/Task/FizzBuzz_Test.java
@@ -0,0 +1,8 @@
+package Task;
+
+public class FizzBuzz_Test {
+//    Write a program that prints numbers from 1 to 100. However, for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+static void main() {
+
+}
+}
