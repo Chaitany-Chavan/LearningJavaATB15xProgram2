73c3c7ef01e26af4156fd15106980b016841a4d3
Chaitany_Chavan <chaitanychavan993@gmail.com>
2026-02-14 17:46:16 +0530

    14 feb 2026 Update

---
+++ src/ex_22_OOPs__Ecap_AccessModifer/Police/JrCop.java
@@ -0,0 +1,9 @@
+package ex_22_OOPs__Ecap_AccessModifer.Police;
+
+public class JrCop {
+    public static void main(String[] args) {
+
+        Cop jrCop = new Cop(5);
+        jrCop.canIShoot();
+    }
+}
